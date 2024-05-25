559a911760cd89a1099cba0a8837bd24fb4dbf7b
Exquisite-Vulpes <foxdity25@gmail.com>
2024-05-25 04:26:33 +0000

    first commit

---
+++ Dossier de codage/index.js
@@ -0,0 +1,18 @@
+console.log(`hello`);
+console.log(`Hola`);
+
+
+//window.alert(`Alert`);
+
+//one line comment
+
+/*multi line comment*/
+
+//document.getElementById("text2").textContent = `hi` ;
+
+let x;
+x = 100
+
+let Word = "Bird"
+
+console.log(`you are ${Word} `);
